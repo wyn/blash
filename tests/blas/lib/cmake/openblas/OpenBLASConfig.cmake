855633fea55e77f3dd0a125f797b6399011a29cb
Simon Parry <simon.parry@coshx.co.uk>
2015-12-29 22:46:29 +0000

    removed extra test libs not needed

--- tests/blas/lib/cmake/openblas/OpenBLASConfig.cmake
+++
@@ -1,3 +0,0 @@
-SET(OpenBLAS_VERSION "0.2.16.dev")
-SET(OpenBLAS_INCLUDE_DIRS /Users/admin/dev/haskell/vec/tests/blas/include)
-SET(OpenBLAS_LIBRARIES /Users/admin/dev/haskell/vec/tests/blas/lib/libopenblas.dylib)
