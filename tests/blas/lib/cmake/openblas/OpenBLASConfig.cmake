f0b0f148ece2e8bb2536d1096803e967ebd36180
Simon Parry <simon.parry@coshx.co.uk>
2015-12-31 07:44:45 +0000

    Revert "removed extra test libs not needed"
    This reverts commit 855633fea55e77f3dd0a125f797b6399011a29cb.

---
+++ tests/blas/lib/cmake/openblas/OpenBLASConfig.cmake
@@ -0,0 +1,3 @@
+SET(OpenBLAS_VERSION "0.2.16.dev")
+SET(OpenBLAS_INCLUDE_DIRS /Users/admin/dev/haskell/vec/tests/blas/include)
+SET(OpenBLAS_LIBRARIES /Users/admin/dev/haskell/vec/tests/blas/lib/libopenblas.dylib)
---
+++ tests/nlopt/slsqp.h
@@ -0,0 +1,26 @@
+#ifndef SLSQP_H
+#define SLSQP_H
+
+/* #include "nlopt.h" */
+/* #include "nlopt-util.h" */
+void dcopy___(int *n_,
+              const double *dx, int incx,
+              double *dy, int incy);
+
+
+/* #ifdef __cplusplus */
+/* extern "C" */
+/* { */
+/* #endif /\* __cplusplus *\/ */
+
+/* nlopt_result nlopt_slsqp(unsigned n, nlopt_func f, void *f_data, */
+/* 			 unsigned m, nlopt_constraint *fc, */
+/* 			 unsigned p, nlopt_constraint *h, */
+/* 			 const double *lb, const double *ub, */
+/* 			 double *x, double *minf, */
+/* 			 nlopt_stopping *stop); */
+/* #ifdef __cplusplus */
+/* }  /\* extern "C" *\/ */
+/* #endif /\* __cplusplus *\/ */
+
+#endif
